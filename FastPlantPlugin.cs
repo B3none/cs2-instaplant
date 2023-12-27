8552a0f8c39d2ee2b4361e73cf9c6f30a9b88904
B3none <ablackham2000@gmail.com>
2023-12-27 00:36:57 +0000

    Added initial plugin files

---
+++ FastPlantPlugin.cs
@@ -0,0 +1,21 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes;
+using CounterStrikeSharp.API.Modules.Utils;
+using System.Numerics;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+
+namespace FastPlantPlugin;
+
+[MinimumApiVersion(129)]
+public class FastPlantPlugin : BasePlugin
+{
+    private const string Version = "1.0.0";
+    
+    public override string ModuleName => "Fast Plant Plugin";
+    public override string ModuleVersion => Version;
+    public override string ModuleAuthor => "B3none";
+    public override string ModuleDescription => "Allows a CT to instantly defuse the bomb when all Ts are dead and nothing can prevent the defusal.";
+    
+    
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# cs2-fastplant
-Allows terrorists to plant the bomb instantly
+# CS2 Fast Plant
+Plant the bomb instantly as a terrorist.
+
+## Setup for development
+Download the latest release of CounterStrikeSharp, and put the contents into the `CounterStrikeSharp` directory.
+
+## Share the love
+If you appreciate the project then please take the time to star the repository.
+
+![Star us](https://github.com/b3none/gdprconsent/raw/development/.github/README_ASSETS/star_us.png)
